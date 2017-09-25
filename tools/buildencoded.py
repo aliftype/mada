c63008d41f5657360fa74073adfb21df388a7d53
Khaled Hosny <khaledhosny@eglug.org>
2017-09-25 23:50:07 +0200

    Simplify placeholder glyphs
    Just draw a 2x2 square. This should be completely dropped once next
    FontConfig is released.

--- tools/buildencoded.py
+++ tools/buildencoded.py
@@ -1,12 +1,11 @@
 import sys
 import os
 
-from defcon import Font, Component
 from fontTools.misc.py23 import *
 from fontTools.feaLib.parser import Parser
 
 def parse(features):
-    subs = {}
+    subs = set()
     featurefile = UnicodeIO(tounicode(features))
     fea = Parser(featurefile, []).parse()
     for statement in fea.statements:
@@ -14,43 +13,24 @@ def parse(features):
             for substatement in statement.statements:
                 if hasattr(substatement, "glyphs"):
                     # Single
-                    originals = substatement.glyphs[0].glyphSet()
-                    replacements = substatement.replacements[0].glyphSet()
-                    subs.update(dict(zip(originals, replacements)))
+                    subs.update(substatement.glyphs[0].glyphSet())
                 elif hasattr(substatement, "glyph"):
                     # Multiple
-                    subs[substatement.glyph] = substatement.replacement
+                    subs.add(substatement.glyph)
 
     return subs
-
-def addComponent(glyph, name, xoff=0, yoff=0):
-    component = glyph.instantiateComponent()
-    component.baseGlyph = name
-    component.move((xoff, yoff))
-    glyph.appendComponent(component)
 
 def build(font):
     subs = parse(font.features.text)
 
-    for name, names in subs.items():
-        if isinstance(names, (str, unicode)):
-            names = [names]
-        baseGlyph = font[names[0]]
+    for name in subs:
         glyph = font.newGlyph(name)
         glyph.unicode = int(name.lstrip('uni'), 16)
-        glyph.width = baseGlyph.width
-        glyph.leftMargin = baseGlyph.leftMargin
-        glyph.rightMargin = baseGlyph.rightMargin
-        addComponent(glyph, baseGlyph.name)
-        for partName in names[1:]:
-            partGlyph = font[partName]
-            partAnchors = [a.name.replace("_", "", 1) for a in partGlyph.anchors if a.name.startswith("_")]
-            baseAnchors = [a.name for a in baseGlyph.anchors if not a.name.startswith("_")]
-            anchorName = set(baseAnchors).intersection(partAnchors)
-            assert len(anchorName) > 0, (names[0], partName, partAnchors, baseAnchors)
-            anchorName = list(anchorName)[0]
-            partAnchor = [a for a in partGlyph.anchors if a.name == "_" + anchorName][0]
-            baseAnchor = [a for a in baseGlyph.anchors if a.name == anchorName][0]
-            xoff = baseAnchor.x - partAnchor.x
-            yoff = baseAnchor.y - partAnchor.y
-            addComponent(glyph, partName, xoff, yoff)
+        glyph.width = glyph.leftMargin = glyph.rightMargin = 0
+        # TODO, remove this once FontConfig is updated
+        pen = glyph.getPen()
+        pen.moveTo((-1, -1))
+        pen.lineTo(( 1, -1))
+        pen.lineTo(( 1,  1))
+        pen.lineTo((-1,  1))
+        pen.closePath()
