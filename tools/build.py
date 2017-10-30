aliftype/mada

commit a367974b61ac6d6d8ef8093fbdd0adab5a7dfd55

Initial slant axis support

tools/build.py
@@ -8,10 +8,9 @@ from fontmake.font_project import FontProject
 from mutatorMath.ufo.document import DesignSpaceDocumentReader
 
 def epoch(args):
-    designspace = os.path.join(args.source, args.designspace)
-    reader = DesignSpaceDocumentReader(designspace, ufoVersion=3)
-    paths = reader.getSourcePaths() + [designspace]
-    epoch = max([os.stat(p).st_mtime for p in paths])
+    reader = DesignSpaceDocumentReader(args.designspace, ufoVersion=3)
+    paths = reader.getSourcePaths() + [args.designspace]
+    epoch = max([os.stat(os.path.join(args.source, p)).st_mtime for p in paths])
 
     return str(int(epoch))
 

commit d49ee34b17e3432ac79b9313999b333058087c69

Fix modification time calculation
I broke it in a367974b61ac6d6d8ef8093fbdd0adab5a7dfd55.

tools/build.py
@@ -10,7 +10,11 @@ from mutatorMath.ufo.document import DesignSpaceDocumentReader
 def epoch(args):
     reader = DesignSpaceDocumentReader(args.designspace, ufoVersion=3)
     paths = reader.getSourcePaths() + [args.designspace]
-    epoch = max([os.stat(os.path.join(args.source, p)).st_mtime for p in paths])
+    # We want to check the original masters in the source not build directory.
+    paths = [os.path.join(args.source, os.path.basename(p)) for p in paths]
+    # Not all masters exist in the source directory.
+    paths = [p for p in paths if os.path.exists(p)]
+    epoch = max([os.stat(p).st_mtime for p in paths])
 
     return str(int(epoch))
 